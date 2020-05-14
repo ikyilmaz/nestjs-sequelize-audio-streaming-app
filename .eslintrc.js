302aa2e29d3097250e49aa961339816ba326359e
ismail <ismailkurbanyilmaz@gmail.com>
2020-05-14 18:15:57 +0300

    some update

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,7 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'max-len': 130,
+    'arrow-parens': true,
   },
 };
