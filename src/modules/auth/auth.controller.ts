5fb0ed17e58cd99d0e413306dac4aaa5ab5ed8d3
ismail <ismailkurbanyilmaz@gmail.com>
2020-05-14 22:38:18 +0300

    js-docs added for auth module

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -20,22 +20,41 @@ import { catchAsync } from '../../helpers/utils/catch-async';
 import { SignUpDto } from './dto/sign-up.dto';
 import { filterObject } from '../../helpers/utils/filter-object';
 import { SignInDto } from './dto/sign-in.dto';
+import {
+    ApiBadRequestResponse, ApiBearerAuth,
+    ApiCreatedResponse,
+    ApiOkResponse,
+    ApiOperation,
+    ApiTags,
+    ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 @Controller('auth')
+@ApiTags('auth')
 export class AuthController {
-    constructor(private readonly $authService: AuthService, private readonly $tokenService: TokenService) {}
-
-    @Post('/sign-up')
-    async signUp(@Body() body: SignUpDto, @Res() res: Response) {
-        if (body.password != body.passwordConfirm) throw new BadRequestException("passwords don't match");
-
-        const doc = await catchAsync(this.$authService.create(body));
-
-        filterObject(doc, ['password', 'passwordChangedAt']);
-
-        await this.createAndSendToken(doc, res);
+    constructor(private readonly $authService: AuthService, private readonly $tokenService: TokenService) {
     }
 
+    /**
+     *  @description Sign up path, creates a user and returns it
+     *  @statusCodes 200, 400 */
+    @ApiOperation({ summary: 'SIGN UP' })
+    @ApiCreatedResponse({ description: 'User created.' })
+    @ApiBadRequestResponse({ description: 'Validation failed.' })
+    @Post('/sign-up')
+    async signUp(@Body() body: SignUpDto, @Res() res: Response) {
+        if (body.password != body.passwordConfirm) throw new BadRequestException('passwords don\'t match');
+
+        await this.createAndSendToken(await catchAsync(this.$authService.create(body)), res);
+    }
+
+    /**
+     *  @description Sign in path, finds the user with the given credentials and returns it
+     *  @statusCodes 200, 401, 400 */
+    @ApiOperation({ summary: 'SIGN IN' })
+    @ApiOkResponse({ description: 'Signed in.' })
+    @ApiUnauthorizedResponse({ description: 'Incorrect credentials.' })
+    @ApiBadRequestResponse({ description: 'Validation failed.' })
     @Post('/sign-in')
     @HttpCode(HttpStatus.OK)
     async signIn(@Body() body: SignInDto, @Res() res: Response) {
@@ -51,28 +70,65 @@ export class AuthController {
         await this.createAndSendToken(doc, res);
     }
 
+    /**
+     *  @description Sign out path, removes the jwt from the cookies
+     *  @statusCodes 200 */
+    @ApiOperation({ summary: 'SIGN OUT' })
+    @ApiOkResponse({ description: 'Signed out.' })
     @Get('/sign-out')
     async signOut(@Res() res: Response) {
-        res.cookie('jwt', 'signed-out', {
-            httpOnly: true,
-            expires: moment()
-                .add(15, 'seconds')
-                .toDate(),
-        }).send();
+        res.cookie('jwt', 'signed-out', { httpOnly: true, expires: moment().add(15, 'seconds').toDate() }).send();
     }
 
+    /**
+     *  @description Updates user's password
+     *  @permissions authenticated users
+     *  @statusCodes 200, 401, 400 */
+    @ApiOperation({ summary: 'UPDATE PASSWORD' })
+    @ApiOkResponse({ description: 'Password updated.' })
+    @ApiUnauthorizedResponse({ description: 'Auth required.' })
+    @ApiBadRequestResponse({ description: 'Validation failed.' })
     @Patch('/update-password')
-    async updatePassword() {}
+    async updatePassword() {
 
+    }
+
+    /**
+     *  @description Updates user's email, sends verification email
+     *  @permissions authenticated users
+     *  @statusCodes 200, 401, 400 */
+    @ApiOperation({ summary: 'UPDATE EMAIL' })
+    @ApiOkResponse({ description: 'Verification email sent.' })
+    @ApiUnauthorizedResponse({ description: 'Auth required.' })
+    @ApiBadRequestResponse({ description: 'Validation failed.' })
     @Patch('/update-email')
-    async updateEmail() {}
+    async updateEmail() {
 
+    }
+
+    /**
+     *  @description Updates user's some properties
+     *  @permissions authenticated users
+     *  @statusCodes 200, 401, 400 */
+    @ApiOperation({ summary: 'UPDATE USER' })
+    @ApiOkResponse({ description: 'User updated.' })
+    @ApiUnauthorizedResponse({ description: 'Auth required.' })
+    @ApiBadRequestResponse({ description: 'Validation failed.' })
     @Patch('/update')
-    async updateMe() {}
+    async updateMe() {
 
+    }
+
+    /**
+     *  @description Returns the current user if it exists, if not returns null
+     *  @permissions authenticated users
+     *  @statusCodes 200 */
+    @ApiOperation({ summary: 'GET CURRENT USER' })
+    @ApiOkResponse({ description: 'Current user found or not.' })
     @Get('/current-user')
-    @HttpCode(HttpStatus.OK)
-    async getCurrentUser() {}
+    async getCurrentUser() {
+
+    }
 
     private async createAndSendToken(user: User, res: Response) {
         const token = await catchAsync(this.$tokenService.generateJWTToken(user.id));
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import User from '../../models/user/user.model';
+import { TokenModule } from '@app/token';
 
 @Module({
+    imports: [SequelizeModule.forFeature([User]), TokenModule],
     controllers: [AuthController],
     providers: [AuthService],
 })
