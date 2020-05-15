49368d7fe808693c4ca0dbdec8949b66ffb4f7ca
ismail <ismailkurbanyilmaz@gmail.com>
2020-05-15 03:27:51 +0300

    is-owner.guard.ts added

---
+++ src/guards/is-owner.guard.ts
@@ -0,0 +1,28 @@
+import { CanActivate, ExecutionContext, Injectable, NotFoundException } from '@nestjs/common';
+import { CurrentUserService } from '@app/current-user';
+import { Reflector } from '@nestjs/core';
+import { Request } from 'express';
+import { ModelCtor } from 'sequelize-typescript';
+
+@Injectable()
+export class IsOwnerGuard implements CanActivate {
+    constructor(private $currentUserService: CurrentUserService, private $reflector: Reflector) {
+
+    }
+
+    async canActivate(context: ExecutionContext) {
+        const req = context.switchToHttp().getRequest<Request>();
+
+        const model = this.$reflector.get<ModelCtor>('model', context.getHandler());
+
+        const doc = await model.findOne({ where: req.params });
+
+        if (!doc) throw new NotFoundException();
+
+        // @ts-ignore
+        const isOwner = this.$currentUserService.getUser.id == doc.userId;
+        const isAdmin = this.$currentUserService.getUser.role == 'admin';
+
+        return (isOwner || isAdmin);
+    }
+}
