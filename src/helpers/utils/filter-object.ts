5ff0ea69bbfa36b2f9cb0fa2dc957d5f7724d070
ismail <ismailkurbanyilmaz@gmail.com>
2020-05-15 03:28:05 +0300

    refactor

--- src/helpers/utils/filter-object.ts
+++ src/helpers/utils/filter-object.ts
@@ -1,12 +1,15 @@
 /**
  * @description removes unwanted fields in the given object
  * */
-export const filterObject = (obj: Pick<any, any>, fields: string[], allowed: boolean = false) => {
+export const filterObject = <T extends object>(obj: T, fields: string[], allowed: boolean = false) => {
     if (allowed) {
-        const newObj: Pick<any, any> = {};
+        //@ts-ignore
+        const newObj: T = {};
         Object.keys(obj).forEach(el => (fields.includes(el) ? (newObj[el] = obj[el]) : null));
         return newObj;
     }
 
-    return fields.forEach(field => (obj[field] = undefined));
+    fields.forEach(field => obj[field] = undefined);
+
+    return obj
 };
