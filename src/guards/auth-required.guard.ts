cf08bf695e04a35c4b765fe080e58760568ab5da
ismail <ismailkurbanyilmaz@gmail.com>
2020-05-15 03:27:32 +0300

    auth-required.guard.ts added

--- src/guards/auth-required.guard.ts
+++ src/guards/auth-required.guard.ts
@@ -1,15 +1,15 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Request } from 'express';
-import { TokenService } from '@app/token';
 import { CurrentUserService } from '@app/current-user';
 import { InjectModel } from '@nestjs/sequelize';
 import User from '../models/user/user.model';
 import { catchAsync } from '../helpers/utils/catch-async';
+import { promisify } from "util";
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class AuthRequiredGuard implements CanActivate {
     constructor(
-        private $tokenService: TokenService,
         private $currentUserService: CurrentUserService,
         @InjectModel(User) private $user: typeof User,
     ) {}
@@ -27,7 +27,8 @@ export class AuthRequiredGuard implements CanActivate {
 
         if (!token) return false;
 
-        const decoded = await catchAsync(this.$tokenService.verifyJWTToken(token));
+        //@ts-ignore TODO
+        const decoded = await catchAsync(await promisify(jwt.verify)(token, process.env.JWT_SECRET) as Promise<{ id: string; iat: string }>);
 
         if (!decoded.id) return false;
 
