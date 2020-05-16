5bcb1eabc1911d5719b24663e59e9bc88e72d50b
ismail <ismailkurbanyilmaz@gmail.com>
2020-05-16 23:12:23 +0300

    refactor

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -5,11 +5,12 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import UserProfile from '../../models/user/user-profile/user-profile.model';
 import { filterObject } from '../../helpers/utils/filter-object';
-import { limitFields, paginate } from '../../helpers/utils/api-features';
+import { paginate } from '../../helpers/utils/api-features';
 import { queryObject } from '../../helpers/utils/query-object';
 import { UserFields as uf } from '../../models/user/user.enums';
 import { GetManyUserQueryDto } from './dto/user-query.dto';
 import { GetOneQueryDto } from '../../helpers/common-dtos/common-query.dto';
+import { limitPublicUserFields } from '../../helpers/field-limiters/user.field-limiters';
 
 @Injectable()
 export class UsersService {
@@ -20,36 +21,25 @@ export class UsersService {
     getMany = (query: GetManyUserQueryDto) => {
         return this.$user.scope('public').findAll({
             ...paginate(query),
-            attributes: limitFields(query.fields, {
-                _enum: uf,
-                defaults: [uf.id, uf.firstName, uf.lastName, uf.username],
-                disallowedFields: [uf.password, uf.passwordChangedAt, uf.email, uf.role, uf.createdAt, uf.updatedAt],
-            }),
+            attributes: limitPublicUserFields(query.fields),
             where: queryObject(query, [uf.firstName, uf.lastName, uf.username, uf.photo]),
         });
     };
 
-    get = (query: GetOneQueryDto,id: string) => {
+    get = (query: GetOneQueryDto, id: string) => {
         return this.$user.scope('public').findByPk(id, {
-            attributes: limitFields(query.fields, {
-                _enum: uf,
-                defaults: [uf.id, uf.firstName, uf.lastName, uf.username],
-                disallowedFields: [uf.password, uf.passwordChangedAt, uf.email, uf.role, uf.createdAt, uf.updatedAt],
-            }),
+            attributes: limitPublicUserFields(query.fields),
         });
     };
 
     create = async (createUserDto: CreateUserDto) => {
         const user = await this.$user.create({ ...createUserDto, profile: {} }, { include: [UserProfile] });
-        filterObject(user, ['password', 'email']);
-        return user;
+        return filterObject(user, ['password', 'email']);
     };
 
-    update = async (id: string, updateUserDto: UpdateUserDto) => {
-        await this.$user.update(updateUserDto, { where: { id } });
-    };
+    update = async (id: string, updateUserDto: UpdateUserDto) => this.$user.update(updateUserDto, { where: { id } });
 
-    delete = (id: string) => {
-        return this.$user.destroy({ where: { id } });
-    };
+
+    delete = (id: string) => this.$user.destroy({ where: { id } });
+
 }
