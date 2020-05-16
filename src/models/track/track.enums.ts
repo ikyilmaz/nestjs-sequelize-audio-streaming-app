554181eaddbacfadd669a9433883f9dcd34cd156
ismail <ismailkurbanyilmaz@gmail.com>
2020-05-16 03:51:24 +0300

    track.enums.ts added

---
+++ src/models/track/track.enums.ts
@@ -0,0 +1,9 @@
+export enum TrackFields {
+    id = 'id',
+    title = 'title',
+    track = 'track',
+    albumId = 'albumId',
+    ownerId = 'ownerId',
+    createdAt = 'createdAt',
+    updatedAt = 'updatedAt'
+}
