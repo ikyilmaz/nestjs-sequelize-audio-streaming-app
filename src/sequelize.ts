98127ad58ba02c6ba809741bbcea9f1499701a97
ismail <ismailkurbanyilmaz@gmail.com>
2020-05-18 01:47:59 +0300

    models added

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -1,32 +1,41 @@
 import User from './models/user/user.model';
 import Track from './models/track/track.model';
 import Album from './models/album/album.model';
-import UserProfile from './models/user/user-profile/user-profile.model';
-import Friendship from './models/user/friendship/friendship.model';
-import UserTrack from './models/m2m/usertrack.model';
-import UserAlbum from './models/m2m/useralbum.model';
+import UserProfile from './models/user-profile/user-profile.model';
+import Friendship from './models/m2m/friendship/friendship.model';
+import FeaturingTrack from './models/m2m/featuring/featuring-track/featuring-track.model';
+import FeaturingAlbum from './models/m2m/featuring/featuring-album/featuring-album.model';
 import { Injectable } from '@nestjs/common';
 import { SequelizeModuleOptions, SequelizeOptionsFactory } from '@nestjs/sequelize';
+import TrackComment from './models/comment/track-comment/track-comment.model';
+import AlbumComment from './models/comment/album-comment/album-comment.model';
+import Playlist from './models/playlist/playlist.model';
+import TrackInThePlaylist from './models/m2m/track-in-the-playlist/track-in-the-playlist.model';
+import Mood from './models/mood/mood.model';
 
 @Injectable()
 export class SequelizeConfigService implements SequelizeOptionsFactory {
     createSequelizeOptions(): SequelizeModuleOptions {
         return {
-
             dialect: process.env.DB_DIALECT as 'mysql',
             username: process.env.DB_USER,
             database: process.env.DB_NAME,
-            sync: { force: true, logging: true },
+            sync: { force: true },
             models: [
                 User,
                 Track,
+                TrackComment,
                 Album,
+                AlbumComment,
                 UserProfile,
                 Friendship,
+                Playlist,
+                TrackInThePlaylist,
+                Mood,
 
                 // Many 2 Many
-                UserTrack,
-                UserAlbum,
+                FeaturingTrack,
+                FeaturingAlbum,
             ],
         };
     }
