cb819c716a5b795a0d4b7da70ea37c1898d83f0b
ismail <ismailkurbanyilmaz@gmail.com>
2020-05-18 01:48:23 +0300

    track-comment.model.ts created and also implemented

---
+++ src/models/comment/track-comment/track-comment.model.ts
@@ -0,0 +1,43 @@
+import { BaseModel } from '../../base';
+import { AllowNull, BelongsTo, Column, DataType, ForeignKey, Length, Table } from 'sequelize-typescript';
+import Track from '../../track/track.model';
+import User from '../../user/user.model';
+
+@Table({ timestamps: true, paranoid: true, tableName: "track_comments" })
+export default class TrackComment extends BaseModel<TrackComment> {
+    /**@description content of the comment*/
+    @Length({ min: 1, max: 255, msg: 'field \'content\' must be between 1 and 255 characters' })
+    @AllowNull(false)
+    @Column(DataType.STRING)
+    content: string;
+
+    /**@description */
+    @Column(DataType.SMALLINT)
+    minuteOfTheMusic: number;
+
+    // --> ASSOCIATIONS
+
+    // --> TRACK
+
+    /**@description track id*/
+    @ForeignKey(() => Track)
+    @AllowNull(false)
+    @Column(DataType.UUID)
+    trackId: string;
+
+    /**@description* track itself*/
+    @BelongsTo(() => Track)
+    track: Track;
+
+    // --> OWNER (USER)
+
+    /**@description owner id*/
+    @ForeignKey(() => User)
+    @AllowNull(false)
+    @Column(DataType.UUID)
+    ownerId: string;
+
+    /**@description* owner itself*/
+    @BelongsTo(() => User)
+    owner: User;
+}
