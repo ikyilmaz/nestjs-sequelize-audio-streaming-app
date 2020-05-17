830579d12fd849a3e307af7dc7f41571aef6a697
ismail <ismailkurbanyilmaz@gmail.com>
2020-05-18 01:50:31 +0300

    associations specified

--- src/models/user/user.model.ts
+++ src/models/user/user.model.ts
@@ -18,12 +18,16 @@ import { STRING, SaveOptions, UpdateOptions } from 'sequelize';
 import { BaseModel } from '../base';
 import { hash } from 'bcryptjs';
 import Album from '../album/album.model';
-import UserAlbum from '../m2m/useralbum.model';
+import FeaturingAlbum from '../m2m/featuring/featuring-album/featuring-album.model';
 import Track from '../track/track.model';
-import UserTrack from '../m2m/usertrack.model';
-import Friendship from './friendship/friendship.model';
+import FeaturingTrack from '../m2m/featuring/featuring-track/featuring-track.model';
+import Friendship from '../m2m/friendship/friendship.model';
 import { userScopes } from './user.scopes';
-import UserProfile from './user-profile/user-profile.model';
+import UserProfile from '../user-profile/user-profile.model';
+import TrackComment from '../comment/track-comment/track-comment.model';
+import AlbumComment from '../comment/album-comment/album-comment.model';
+import Playlist from '../playlist/playlist.model';
+import TrackInThePlaylist from '../m2m/track-in-the-playlist/track-in-the-playlist.model';
 
 @Table({
     timestamps: true,
@@ -31,7 +35,7 @@ import UserProfile from './user-profile/user-profile.model';
     scopes: userScopes,
 })
 export default class User extends BaseModel<User> {
-    /*** @description User's first name, not required */
+    /** @description User's first name, not required */
     @Length({
         min: 2,
         max: 32,
@@ -40,7 +44,7 @@ export default class User extends BaseModel<User> {
     @Column(STRING(32))
     firstName!: string;
 
-    /*** @description User's last name, not required */
+    /** @description User's last name, not required */
     @Length({
         min: 2,
         max: 32,
@@ -49,7 +53,7 @@ export default class User extends BaseModel<User> {
     @Column(STRING(32))
     lastName!: string;
 
-    /*** @description User's username, must be unique */
+    /** @description User's username, must be unique */
     @Is({
         args: /^[a-z0-9_]{2,32}$/,
         msg: 'field \'username\' must be a valid username',
@@ -59,14 +63,14 @@ export default class User extends BaseModel<User> {
     @Column(STRING(32))
     username!: string;
 
-    /*** @description User's email, must be unique */
+    /** @description User's email, must be unique */
     @IsEmail
     @Unique
     @AllowNull(false)
     @Column(STRING(64))
     email!: string;
 
-    /*** @description User's password */
+    /** @description User's password */
     @AllowNull(false)
     @Column(STRING(128))
     password!: string;
@@ -85,71 +89,95 @@ export default class User extends BaseModel<User> {
     @Column(STRING(128))
     photo!: string;
 
-    /*** @description takes timestamps when the password changes  */
+    /** @description takes timestamps when the password changes  */
     @Column
     passwordChangedAt!: Date;
 
-    // ASSOCIATIONS
+    // --> ASSOCIATIONS
 
-    // PROFILE
+    // --> PROFILE
 
-    /*** @description User's profile */
+    /** @description User's profile */
     @HasOne(() => UserProfile)
     profile!: UserProfile;
 
-    // ALBUMS
+    // --> ALBUMS
 
-    /*** @description User's albums */
+    /** @description User's albums */
     @HasMany(() => Album, { foreignKey: 'ownerId' })
     albumsOwned!: Album[];
 
-    /***@description Albums which ones are participated by user */
+    /** @description Albums which ones are participated by user */
     @BelongsToMany(() => Album, {
-        through: { model: () => UserAlbum, unique: false },
+        through: { model: () => FeaturingAlbum, unique: false },
     })
     albumsParticipated!: Album[];
 
-    // TRACKS
+    // --> TRACKS
 
-    /***@description User's tracks*/
+    /** @description User's tracks*/
     @HasMany(() => Track)
     tracksOwned!: Track;
 
-    /***@description Tracks participated by user */
+    /** @description Tracks participated by user */
     @BelongsToMany(() => Track, {
-        through: { model: () => UserTrack, unique: false },
+        through: { model: () => FeaturingTrack, unique: false },
     })
     tracksParticipated!: Track[];
 
-    // FRIENDS
+    // --> FRIENDS
 
-    /***@description User's friends (users following by the current user) */
+    /** @description User's friends (users following by the current user) */
     @BelongsToMany(() => User, {
         through: { model: () => Friendship, unique: false },
         foreignKey: 'friendId',
     })
     friends!: User[];
 
-    // FOLLOWERS
+    // --> FOLLOWERS
 
-    /***@description User's followers (users following to the current user)*/
+    /** @description User's followers (users following to the current user)*/
     @BelongsToMany(() => User, {
         through: { model: () => Friendship, unique: false },
         foreignKey: 'ownerId',
     })
     followers!: User[];
 
-    // HOOKS
+    // --> TRACK COMMENTS
 
-    /***@description Hook for hashing the password before save */
+    /** @description Track comments*/
+    @HasMany(() => TrackComment)
+    trackComments: TrackComment[];
+
+    // --> ALBUM COMMENTS
+
+    /** @description Album comments */
+    @HasMany(() => AlbumComment)
+    albumComments: AlbumComment[];
+
+    // --> PLAYLISTS
+
+    /** @description Playlists */
+    @HasMany(() => Playlist)
+    playlists: Playlist[];
+
+    // -->  OWNER OF THE TRACKS IN PLAYLISTS
+
+    /** @description mind fuck */
+    @HasMany(() => TrackInThePlaylist)
+    tracksInPlaylists: TrackInThePlaylist[];
+
+    // --> HOOKS
+
+    /** @description Hook for hashing the password before save */
     @BeforeSave({ name: 'hash-password-before-save' })
-    static async hashPasswordBeforeSave(user: User, options: SaveOptions) {
+    static async hashPasswordBeforeSave(user: User, _: SaveOptions) {
         user.password = await hash(user.password, 12);
     }
 
-    /***@description Hook for hashing the password if it exists before update */
+    /** @description Hook for hashing the password if it exists before update */
     @BeforeUpdate({ name: 'hash-password-before-update' })
-    static async hashPasswordBeforeUpdate(user: User, options: UpdateOptions) {
+    static async hashPasswordBeforeUpdate(user: User, _: UpdateOptions) {
         if (user.password) user.password = await hash(user.password, 12);
     }
 }
