b6c71322433950ff5d29bc840b5cc1f1865727c1
ismail <ismailkurbanyilmaz@gmail.com>
2020-05-18 01:48:32 +0300

    track-in-the-playlist.model.ts created and also implemented

---
+++ src/models/m2m/track-in-the-playlist/track-in-the-playlist.model.ts
@@ -0,0 +1,46 @@
+import { BaseModel } from '../../base';
+import { AllowNull, BelongsTo, Column, DataType, ForeignKey, Table } from 'sequelize-typescript';
+import Track from '../../track/track.model';
+import User from '../../user/user.model';
+import Playlist from '../../playlist/playlist.model';
+
+@Table({ timestamps: true, updatedAt: false, tableName: 'tracks_in_the_playlist' })
+export default class TrackInThePlaylist extends BaseModel<TrackInThePlaylist> {
+    // --> ASSOCIATIONS
+
+    // --> PLAYLIST
+
+    /**@description playlist id*/
+    @ForeignKey(() => Playlist)
+    @AllowNull(false)
+    @Column(DataType.UUID)
+    playlistId: string;
+
+    /**@description* playlist itself*/
+    @BelongsTo(() => Playlist)
+    playlist: Playlist;
+
+    // --> TRACK
+
+    /**@description track id*/
+    @ForeignKey(() => Track)
+    @AllowNull(false)
+    @Column(DataType.UUID)
+    trackId: string;
+
+    /**@description* track itself*/
+    @BelongsTo(() => Track)
+    track: Track;
+
+    // --> OWNER (USER)
+
+    /**@description owner id*/
+    @ForeignKey(() => User)
+    @AllowNull(false)
+    @Column(DataType.UUID)
+    ownerId: string;
+
+    /**@description* owner itself*/
+    @BelongsTo(() => User)
+    owner: User;
+}
