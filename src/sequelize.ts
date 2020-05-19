1f7832b50d8c6d4b07f27a24fece3d2d7fed28a6
ikyilmaz <ismailkurbanyilmaz@gmail.com>
2020-05-19 23:39:26 +0300

    new models added

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -3,15 +3,17 @@ import Track from './models/track/track.model';
 import Album from './models/album/album.model';
 import UserProfile from './models/user-profile/user-profile.model';
 import Friendship from './models/m2m/friendship/friendship.model';
-import FeaturingTrack from './models/m2m/featuring/featuring-track/featuring-track.model';
-import FeaturingAlbum from './models/m2m/featuring/featuring-album/featuring-album.model';
+import TrackFeaturing from './models/m2m/featuring/track-featuring/track-featuring.model';
+import AlbumFeaturing from './models/m2m/featuring/album-featuring/album-featuring.model';
 import { Injectable } from '@nestjs/common';
 import { SequelizeModuleOptions, SequelizeOptionsFactory } from '@nestjs/sequelize';
 import TrackComment from './models/comment/track-comment/track-comment.model';
 import AlbumComment from './models/comment/album-comment/album-comment.model';
 import Playlist from './models/playlist/playlist.model';
-import TrackInThePlaylist from './models/m2m/track-in-the-playlist/track-in-the-playlist.model';
+import TrackPlaylist from './models/m2m/track-playlist/track-playlist.model';
 import Mood from './models/mood/mood.model';
+import TrackLike from './models/m2m/like/track-like/track-like.model';
+import AlbumLike from './models/m2m/like/album-like/album-like.model';
 
 @Injectable()
 export class SequelizeConfigService implements SequelizeOptionsFactory {
@@ -23,19 +25,24 @@ export class SequelizeConfigService implements SequelizeOptionsFactory {
             sync: { force: true },
             models: [
                 User,
+                Friendship,
+
                 Track,
                 TrackComment,
-                Album,
-                AlbumComment,
-                UserProfile,
-                Friendship,
+                TrackLike,
                 Playlist,
-                TrackInThePlaylist,
+                TrackPlaylist,
+
+                Album,
+                AlbumComment,                                 // dewam ke
+                AlbumLike,
+
+                UserProfile,
                 Mood,
 
                 // Many 2 Many
-                FeaturingTrack,
-                FeaturingAlbum,
+                TrackFeaturing,
+                AlbumFeaturing,
             ],
         };
     }
