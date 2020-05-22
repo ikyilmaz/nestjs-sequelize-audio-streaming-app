62078de23492975a4cff1cb06b0b6f23af58c81e
ismail <ismailkurbanyilmaz@gmail.com>
2020-05-23 02:07:34 +0300

    fixed a bug about deleting comment in related endpoint

--- src/modules/albums/album-comments/album-comments.controller.ts
+++ src/modules/albums/album-comments/album-comments.controller.ts
@@ -1,6 +1,52 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Param } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Auth } from '../../../decorators/auth.decorator';
+import { CreateOperation } from '../../../decorators/operations/create.decorator';
+import { ParamIdDto } from '../../../helpers/common-dtos/param-id.dto';
+import { AddCommentDto } from '../../tracks/track-comments/dto/add-comment.dto';
+import { SendResponse } from '../../../helpers/utils/send-response';
+import { catchAsync } from '../../../helpers/utils/catch-async';
+import { UpdateOperation } from '../../../decorators/operations/update.decorator';
+import { UpdateCommentDto } from '../../tracks/track-comments/dto/update-comment.dto';
+import { DeleteOperation } from '../../../decorators/operations/delete.decorator';
+import { AlbumCommentsService } from './album-comments.service';
+import AlbumComment from '../../../models/comment/album-comment/album-comment.model';
 
 @Controller('albums')
-@ApiTags("albums")
-export class AlbumCommentsController {}
+@ApiTags('albums')
+export class AlbumCommentsController {
+
+    constructor(private readonly $albumCommentsService: AlbumCommentsService) {
+
+    }
+
+    /**
+     * --> ADD COMMENT
+     * @description adds a comment to the specified album
+     * @permissions authenticated users
+     * @statusCodes 204, 400, 404 */
+    @ApiOperation({ summary: 'ADD COMMENT' }) @Auth() @CreateOperation('/:id/comments')
+    async addComment(@Param() params: ParamIdDto, @Body() addCommentDto: AddCommentDto) {
+        return SendResponse(await catchAsync(this.$albumCommentsService.addComment(params.id, addCommentDto)));
+    }
+
+    /**
+     * --> UPDATE COMMENT
+     * @description updates the comment of the specified album
+     * @permissions authenticated users
+     * @statusCodes 204, 400, 404 */
+    @ApiOperation({ summary: 'UPDATE COMMENT' }) @Auth({ isOwner: AlbumComment }) @UpdateOperation('/comments/:id')
+    async updateComment(@Param() params: ParamIdDto, @Body() updateCommentDto: UpdateCommentDto) {
+        return SendResponse(await catchAsync(this.$albumCommentsService.updateComment(params, updateCommentDto)));
+    }
+
+    /**
+     * --> REMOVE COMMENT
+     * @description removes a comment to the specified album
+     * @permissions authenticated users
+     * @statusCodes 204, 400, 404 */
+    @ApiOperation({ summary: 'REMOVE COMMENT' }) @Auth({ isOwner: AlbumComment }) @DeleteOperation('/comments/:id')
+    async removeComment(@Param() params: ParamIdDto) {
+        await catchAsync(this.$albumCommentsService.removeComment(params.id));
+    }
+}
--- src/modules/albums/album-comments/album-comments.service.ts
+++ src/modules/albums/album-comments/album-comments.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { CurrentUser } from '@app/current-user';
+import { AddCommentDto } from '../../tracks/track-comments/dto/add-comment.dto';
+import { ParamIdDto } from '../../../helpers/common-dtos/param-id.dto';
+import { UpdateCommentDto } from '../../tracks/track-comments/dto/update-comment.dto';
+import AlbumComment from '../../../models/comment/album-comment/album-comment.model';
 
 @Injectable()
-export class AlbumCommentsService {}
+export class AlbumCommentsService {
+    constructor(
+        @InjectModel(AlbumComment) private readonly $albumComment: typeof AlbumComment,
+        private readonly $currentUser: CurrentUser,
+    ) {
+    }
+
+    addComment(id: string, addCommentDto: AddCommentDto) {
+        return this.$albumComment.create({
+            ownerId: this.$currentUser.getUser.id,
+            albumId: id,
+            content: addCommentDto.content,
+        });
+    }
+
+    updateComment({ id }: ParamIdDto, { content }: UpdateCommentDto) {
+        return this.$albumComment.update({
+            content: content ? content : undefined,
+        }, { where: { id }, returning: true });
+
+    }
+
+    removeComment(id: string) {
+        return this.$albumComment.destroy({ where: { id } });
+    }
+}
